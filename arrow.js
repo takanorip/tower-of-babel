b59dda6e5cadad98c9b3bbc237ddef5760ea239b
takanorip <takanori0170@gmail.com>
2016-10-22 16:52:32 +0900

    arrow関数追加

--- arrow.js
+++ arrow.js
@@ -1,4 +1,4 @@
 var inputs = process.argv.slice(2);
-    var result = inputs.map(/* ここにアロー関数を使って一文字だけ取得しましょう */)
-                       .reduce(/* ここにアロー関数を使って文字列結合しましょう */);
-    console.log(result);
+var result = inputs.map(/* ここにアロー関数を使って一文字だけ取得しましょう */)
+.reduce(/* ここにアロー関数を使って文字列結合しましょう */);
+console.log(result);
